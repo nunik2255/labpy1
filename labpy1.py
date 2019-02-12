b5022b3c250ad11ad0dccf138ad30a1b3efe2d01
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-02-12 20:40:37 +0700

    Add files via upload

---
+++ labpy1.py
@@ -0,0 +1,9 @@
+a = input ("Mauskan bilangan pertama")
+b = input ("Masukan bialnagn kedua")
+c = input ("Masukan bialnagn ketiga")
+if a>b and a>c:
+    print ("Bilangan terbesar",a)
+elif b>a and b>c:
+    print ("Bilangan terbesar",b)
+else:
+    print ("Bilangan terbesar",c)
